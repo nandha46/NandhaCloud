905ac0b42406842ebc081f7c50aed60db0d4022e
nandha46 <nandha.kumar790@gmail.com>
2019-11-28 09:54:34 +0530

    edited jsonread

--- Nandha/src/coreJava/JsonRead.java
+++ Nandha/src/coreJava/JsonRead.java
@@ -1,56 +1,53 @@
 package coreJava;
 
 
-
- 
-
 import java.io.FileReader;
 import java.util.Iterator;
 import java.util.Map;
 
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject; 
-import org.json.simple.parser.*; 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
-public class JsonRead 
-{ 
-	public static void main(String[] args) throws Exception 
-	{ 
-		// parsing file "JSONExample.json" 
+public class JsonRead
+{
+	public static void main(String[] args) throws Exception
+	{
+		// parsing file "JSONExample.json"
 		 Object obj = new JSONParser().parse(new FileReader("/home/local/ZOHOCORP/nandha-con426/Documents/Smallbussinessdb extra/Automobil_json.json"));
 
-		
-		// typecasting obj to JSONObject 
-		JSONObject jo = (JSONObject) obj; 
-		
-		
-		String Employees = (String) jo.get("US_SICCODE"); 
-		
-		System.out.println(Employees); 
-		
-		long age = (long) jo.get("age"); 
-		System.out.println(age); 
-		
-		 
-		Map address = ((Map)jo.get("address")); 
-		
-		Iterator<Map.Entry> itr1 = address.entrySet().iterator(); 
-		while (itr1.hasNext()) { 
-			Map.Entry pair = itr1.next(); 
-			System.out.println(pair.getKey() + " : " + pair.getValue()); 
-		} 
-		
-		JSONArray ja = (JSONArray) jo.get("phoneNumbers"); 
-		
-		Iterator itr2 = ja.iterator(); 
-		
-		while (itr2.hasNext()) 
-		{ 
-			itr1 = ((Map) itr2.next()).entrySet().iterator(); 
-			while (itr1.hasNext()) { 
-				Map.Entry pair = itr1.next(); 
-				System.out.println(pair.getKey() + " : " + pair.getValue()); 
-			} 
-		} 
-	} 
-} 
+
+		// typecasting obj to JSONObject
+		JSONObject jo = (JSONObject) obj;
+
+
+		String Employees = (String) jo.get("US_SICCODE");
+
+		System.out.println(Employees);
+
+		long age = (long) jo.get("age");
+		System.out.println(age);
+
+
+		Map address = ((Map)jo.get("address"));
+
+		Iterator<Map.Entry> itr1 = address.entrySet().iterator();
+		while (itr1.hasNext()) {
+			Map.Entry pair = itr1.next();
+			System.out.println(pair.getKey() + " : " + pair.getValue());
+		}
+
+		JSONArray ja = (JSONArray) jo.get("phoneNumbers");
+
+		Iterator itr2 = ja.iterator();
+
+		while (itr2.hasNext())
+		{
+			itr1 = ((Map) itr2.next()).entrySet().iterator();
+			while (itr1.hasNext()) {
+				Map.Entry pair = itr1.next();
+				System.out.println(pair.getKey() + " : " + pair.getValue());
+			}
+		}
+	}
+}
