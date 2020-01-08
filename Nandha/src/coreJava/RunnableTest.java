ca1e819413e96351f890b547d37a6faea8662bf0
Nandha <nandha.kumar790@gmail.com>
2020-01-08 19:25:58 +0530

    added idea intelliJ

---
+++ Nandha/src/coreJava/RunnableTest.java
@@ -0,0 +1,20 @@
+package coreJava;
+
+public class RunnableTest implements Runnable{
+	@Override
+	public void run() {
+		for (int i = 0; i <= 50; i++) {
+			System.out.println(Thread.currentThread().getName() + ": " + i);
+		}
+	}
+	public static void main(String[] args) {
+		Runnable r = new RunnableTest();
+		Thread t1 = new Thread(r);
+		Thread t2 = new Thread(r);
+		Thread t3 = new Thread(r);
+		
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+}
