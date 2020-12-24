cc9cb7ab39c9f20aa9925e5d0d2c8dd2bfa40b29
Nandha <nandha.kumar790@gmail.com>
2020-12-24 09:56:20 +0530

    finalized fileSize

--- Nandha/src/coreJava/FileSize.java
+++ Nandha/src/coreJava/FileSize.java
@@ -1,40 +1,48 @@
 package coreJava;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileSize {
 	
-	static void findSize(long length) {
+	static String findSize(long length) {
 		double size = length/1024;
 		if (size<1) {
-			System.out.println(size*1024+"B");
-		} else if(size/1024<1){
-			System.out.println(size+"KB");
-		} else if(size/1024<1) {
-			System.out.println(size+"MB");
+			System.out.println(size*1024+" B");
+			return  size*1024 + " B";
+		} else if(size>1 && size<1024){
+			System.out.println(Math.round(size)+" KB");
+			return Math.round(size)+" KB";
+		} else if(size/1024>1 && size/1024 <1024) {
+			System.out.println(Math.round(size/1024)+" MB");
+			return Math.round(size/1024)+" MB";
 		} else {
-			System.out.println(size+"GB");
+			size/=1024;
+			System.out.println(String.format("%.2f",size/1024)+" GB");
+			return String.format("%.2f",size/1024)+" GB";
 		}
 	}
-
+		
 	public static void main(String[] args) {
+		File f = new File("C:\\Users\\nandh\\Downloads\\linkedlist.txt");
+		File f2 = new File("C:\\Users\\nandh\\Downloads\\linkedlistCalc.txt");
+		String fileSize;
+		try {
+		FileWriter writer = new FileWriter(f2);
+		Scanner sc = new Scanner(f);
 		
-	//TODO Refactor by num<1 it is optimal
-		
-		long length =21474814L;
-		findSize(length);
-		double num = length/1024;
-		if(num>1) {
-			num=num/1024;
-			if(num>1){
-				num=num/1024;
-				if(num>1){
-					System.out.println(num+"GB");
-				} else{
-					System.out.println(num*1024+"MB");}
-			} else{
-				System.out.println(num*1024 +"KB");}
-
-		} else {
-			System.out.println(num+"B");
+		while(sc.hasNext()) {
+			fileSize = findSize(sc.nextLong());
+			writer.append(fileSize+"\n");
+		}
+		sc.close();
+		writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
