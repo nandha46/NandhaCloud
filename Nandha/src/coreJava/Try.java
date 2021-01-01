7c60ada554c156cfa82620b26647e35b5457a780
Nandha <nandha.kumar790@gmail.com>
2021-01-01 12:17:31 +0530

    minor edits

--- Nandha/src/coreJava/Try.java
+++ Nandha/src/coreJava/Try.java
@@ -1,15 +1,20 @@
 package coreJava;
 
+/**
+ * 
+ * @author Nandhakumar Subramanian
+ *
+ */
 public class Try {
 
 	public static void main(String[] args) {
 		// Code that prints every 498th word
-		int i; int j=0;
-		for(i=0;i<2665;i++) {
+		int j=0;
+		for(int i=0;i<2665;i++) {
 			if(i%498==0) {
-			j++;	System.out.println(j);
+			j++;	
+			System.out.println(j);
 			}
 		}
 	}
-
 }
