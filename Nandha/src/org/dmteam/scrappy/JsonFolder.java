c035412b42bdd020466d27817b46b222e67a2926
Nandha <nandha.kumar790@gmail.com>
2021-01-03 14:33:59 +0530

    cleaned JsonFolder java

--- Nandha/src/org/dmteam/scrappy/JsonFolder.java
+++ Nandha/src/org/dmteam/scrappy/JsonFolder.java
@@ -8,49 +8,52 @@ import java.io.FileWriter;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
 /**
  * @author Nandhakumar Subramanian
  *
  */
 public class JsonFolder {
-	
+
 	public static void writeJson(File file) {
 		try {
-		    String fwrite = file.getAbsolutePath().concat("_modified.csv");
-		    String line; String[] lineArray;
-		    BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()));
-		    BufferedWriter writer = new BufferedWriter(new FileWriter(fwrite));
-		    reader.readLine();
-		    writer.write("Website,Industry,Employee,Revenue\n");
-			while((line =reader.readLine())!=null) {
-		    lineArray = line.split("~");	
-		    String dynamicData = lineArray[2].replaceAll("\"\"","\"");
-		    dynamicData = dynamicData.substring(1);
-		    System.out.println(dynamicData);
-		    dynamicData = dynamicData.substring(0, dynamicData.length()-1);
-		    Object obj= new JSONParser().parse(dynamicData);
-		    
-		   // Object obj= new JSONParser().parse(lineArray[2]);
-		    JSONObject jo = (JSONObject) obj;
-		    String Employees = (String) jo.get("EMPLOYEES");
-		    String Rev = (String) jo.get("REVENUES");
-		    String Revenue= Rev.replaceAll("\\.","");
-		    if(Revenue.contains(",")) {
-		    	continue;
-		    }
-		    System.out.println("Employees: "+Employees+"\nRevenue: "+Revenue+"\nWebsite: "+lineArray[0]+"\nIndustry: "+lineArray[1]+"\n");
-		    writer.write(lineArray[0]+","+lineArray[1]+","+Employees+","+Revenue+"\n");
-			}reader.close(); writer.close();
+			String fwrite = file.getAbsolutePath().concat("_modified.csv");
+			String line;
+			String[] lineArray;
+			BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(fwrite));
+			reader.readLine();
+			writer.write("Website,Industry,Employee,Revenue\n");
+			while ((line = reader.readLine()) != null) {
+				lineArray = line.split("~");
+				String dynamicData = lineArray[2].replaceAll("\"\"", "\"");
+				dynamicData = dynamicData.substring(1, dynamicData.length() - 1);
+				Object obj = new JSONParser().parse(dynamicData);
+				JSONObject jo = (JSONObject) obj;
+				String Employees = (String) jo.get("EMPLOYEES");
+				String Rev = (String) jo.get("REVENUES");
+				String Revenue = Rev.replaceAll("\\.", "");
+				if (Revenue.contains(",")) {
+					continue;
+				}
+				System.out.println("Employees: " + Employees + "\nRevenue: " + Revenue + "\nWebsite: " + lineArray[0]
+						+ "\nIndustry: " + lineArray[1] + "\n");
+				writer.write(lineArray[0] + "," + lineArray[1] + "," + Employees + "," + Revenue + "\n");
+			}
+			reader.close();
+			writer.close();
 		} catch (Exception e) {
-		    e.printStackTrace();
-		    System.out.println("Here");
+			e.printStackTrace();
+			System.out.println("Here");
 		}
 	}
+
 	public static void main(String[] args) throws Exception {
-		String filename= "/home/local/ZOHOCORP/nandha-con426/Documents/Smallbussinessdb extra";
+	//	String filename = "/home/local/ZOHOCORP/nandha-con426/Documents/Smallbussinessdb extra";
+		String filename = "C:\\Users\\nandh\\Downloads";
 		File fil = new File(filename);
-		File[] fArr= fil.listFiles();
-		for(File f:fArr) {
+		File[] fArr = fil.listFiles();
+		for (File f : fArr) {
 			System.out.println(f.getAbsolutePath());
 			writeJson(f);
 		}
