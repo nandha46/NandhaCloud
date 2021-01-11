9fee70d024e49e1e1909061efb6a3dace48cd28e
Nandha <nandha.kumar790@gmail.com>
2021-01-11 11:32:25 +0530

    edited iconUpdate

--- Nandha/src/file/IconUpdate.java
+++ Nandha/src/file/IconUpdate.java
@@ -3,33 +3,24 @@ package file;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.HashMap;
+import java.util.Map;
 /**
  * 
  * @author Nandhakumar Subramanian
  *
  */
 public class IconUpdate {
-
-	private static void goDeep(File file) {
-		FilenameFilter filter = (dir, name) -> !name.contains(".");
-		FilenameFilter iniFilter = (dir, name) -> name.matches("desktop.ini");
-		boolean flag = false;
-		HashMap<File, Boolean> folderMap;
-		//TODO add folders to the map and flag as flase
-		//once visited the folder mark it as true
-		//move the file pointer to one level up
-		// TO avoid loop inside last depth
+	private static Map<File, Boolean> folderMap = new HashMap<File, Boolean>();
+	private static final  FilenameFilter filter = (dir, name) -> !name.contains(".");
+	private static final FilenameFilter iniFilter = (dir, name) -> name.matches("desktop.ini");
+	private static boolean flag = false;
+	
+	private static void listFolder(File file) {
+		
 		if (file.isDirectory()) {
 			File[] folders = file.listFiles(filter);
 			for (File folder : folders) {
-				System.out.println(folder.getAbsolutePath());
-				File[] desktopIni = folder.listFiles(iniFilter);
-				if (desktopIni.length != 0) {
-					System.out.println(desktopIni[0].getName());
-				}
-				while (folder.listFiles(filter).length != 0) {
-					goDeep(folder);
-				}
+				goDeep(folder);
 			}
 		} else {
 			System.out.println("It's a file");
@@ -37,9 +28,28 @@ public class IconUpdate {
 		System.out.println("---------------");
 	}
 
+	private static void goDeep(File folder) {
+		if (folderMap.containsKey(folder)) {
+			folder = folder.getParentFile();
+			if (folderMap.containsValue(true)) {
+				folder.getParentFile();
+			}
+		}
+		folderMap.put(folder, flag);
+		System.out.println(folder.getAbsolutePath());
+		File[] desktopIni = folder.listFiles(iniFilter);
+		if (desktopIni.length != 0) {
+			System.out.println(desktopIni[0].getName());
+		}
+		while (folder.listFiles(filter).length != 0) {
+			folderMap.put(folder, true);
+			goDeep(folder);
+		}		
+	}
+
 	public static void main(String[] args) {
 		System.out.println("------------------------");
-		File file = new File("C:\\Users\\nandh\\Downloads");
-		goDeep(file);
+		File file = new File("C:\\Users\\nandh\\Downloads\\Java\\test");
+		listFolder(file);
 	}
 }
