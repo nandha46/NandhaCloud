6b12adb50f419504c0cc65f0ec2ff47457241188
Nandha <nandha.kumar790@gmail.com>
2021-01-12 10:42:11 +0530

    cleaned iconUpdate

--- Nandha/src/file/IconUpdate.java
+++ Nandha/src/file/IconUpdate.java
@@ -1,55 +1,56 @@
 package file;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
+
 /**
  * 
  * @author Nandhakumar Subramanian
  *
  */
 public class IconUpdate {
-	private static Map<File, Boolean> folderMap = new HashMap<File, Boolean>();
-	private static final  FilenameFilter filter = (dir, name) -> !name.contains(".");
+	private static final String start = "METHOD_START";
+	private static final String end = "METHOD END";
+	private static final FilenameFilter filter = (dir, name) -> !name.contains(".");
 	private static final FilenameFilter iniFilter = (dir, name) -> name.matches("desktop.ini");
-	private static boolean flag = false;
 	
 	private static void listFolder(File file) {
-		
+		System.out.println(start+"\n");
+		Scanner scanner;
 		if (file.isDirectory()) {
 			File[] folders = file.listFiles(filter);
 			for (File folder : folders) {
-				goDeep(folder);
+				//System.out.println(folder.getAbsolutePath());
+				File[] desktopIni = folder.listFiles(iniFilter);
+				if (desktopIni.length != 0) {
+					System.out.println(desktopIni[0].getAbsolutePath());
+					try {
+						scanner = new Scanner(desktopIni[0]);
+						System.out.println(".........................");
+						while (scanner.hasNext()) {
+							System.out.println(scanner.next());
+						}
+						System.out.println("............................");
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					}
+					
+				}
 			}
-		} else {
+		} else if(file.isFile()){
 			System.out.println("It's a file");
 		}
-		System.out.println("---------------");
-	}
-
-	private static void goDeep(File folder) {
-		if (folderMap.containsKey(folder)) {
-			folder = folder.getParentFile();
-			if (folderMap.containsValue(true)) {
-				folder.getParentFile();
-			}
+		
+		else {
+			System.out.println("Invalid Path");
 		}
-		folderMap.put(folder, flag);
-		System.out.println(folder.getAbsolutePath());
-		File[] desktopIni = folder.listFiles(iniFilter);
-		if (desktopIni.length != 0) {
-			System.out.println(desktopIni[0].getName());
-		}
-		while (folder.listFiles(filter).length != 0) {
-			folderMap.put(folder, true);
-			goDeep(folder);
-		}		
+		System.out.println("\n"+end);
 	}
 
 	public static void main(String[] args) {
-		System.out.println("------------------------");
-		File file = new File("C:\\Users\\nandh\\Downloads\\Java\\test");
+		File file = new File("C:\\Users\\nandh\\Downloads\\");
 		listFolder(file);
 	}
 }
