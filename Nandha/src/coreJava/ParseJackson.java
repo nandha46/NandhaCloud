48280506f133550e96fa0ece7f925edc098b76f8
Nandha <nandha.kumar790@gmail.com>
2021-01-23 10:41:22 +0530

    new file ParseJackson

---
+++ Nandha/src/coreJava/ParseJackson.java
@@ -0,0 +1,22 @@
+package coreJava;
+/**
+ * 
+ * @author Nandhakumar Subramanian
+ *
+ */
+public class ParseJackson {
+	private static final String CLASS_NAME = ParseJackson.class.getName();
+	
+	
+	void parse (String json){
+		final String methodName = "parse";
+		
+	}
+	
+	public static void main (String[] args) {
+		ParseJackson p = new ParseJackson();
+		String json = "";
+		p.parse(json);
+		
+	}
+}
