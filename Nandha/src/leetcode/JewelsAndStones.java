c9480de9c0d254d93d1f8f24d2b5d1ea22537d91
Nandha <nandha.kumar790@gmail.com>
2021-02-10 11:17:26 +0530

    solved jewel problems

--- Nandha/src/leetcode/JewelsAndStones.java
+++ Nandha/src/leetcode/JewelsAndStones.java
@@ -11,6 +11,7 @@ Example 1:
 
 Input: jewels = "aA", stones = "aAAbbbb"
 Output: 3
+
 Example 2:
 
 Input: jewels = "z", stones = "ZZ"
@@ -23,10 +24,21 @@ Output: 0
  */
 public class JewelsAndStones {
 	 int numJewelsInStones(String jewels, String stones) {
-	    return 0;    
+		 int ans = 0;
+		 char[] jewelArray = jewels.toCharArray();
+		 char[] stoneArray = stones.toCharArray();
+		 for (char a : jewelArray) {
+			 for (char b : stoneArray) {
+				 if (a == b )
+					 ans++;
+			 }
+		 }
+		 return ans;
 	    }
 	public static void main(String[] args) {
-
+		JewelsAndStones jas = new JewelsAndStones();
+		String jewels = "aA", stones = "aAAbbbb";
+		System.out.println("Number of Stones that are Jewels: " + jas.numJewelsInStones(jewels, stones));
 	}
 
 }
