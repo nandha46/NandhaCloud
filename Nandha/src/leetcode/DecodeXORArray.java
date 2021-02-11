75e9d0ee7faa3afeb5af524046f2b7274416f4db
Nandha <nandha.kumar790@gmail.com>
2021-02-11 16:44:48 +0530

    solved leetcode problem

--- Nandha/src/leetcode/DecodeXORArray.java
+++ Nandha/src/leetcode/DecodeXORArray.java
@@ -1,13 +1,26 @@
 package leetcode;
+/**
+ * @author Nandhakumar Subramanian
+ */
+import java.util.Arrays;
 
 public class DecodeXORArray {
-	
+
 	int[] decode(int[] encoded, int first) {
-    return encoded;    
-    }
+		int[] ans = new int[encoded.length + 1];
+		ans[0] = first;
+		for (int i = 1; i < encoded.length + 1; i++) {
+			ans[i] = encoded[i-1] ^ first;
+			first = ans[i];
+		}
+		return ans;
+	}
 
 	public static void main(String[] args) {
-
+		DecodeXORArray dxa = new DecodeXORArray();
+		int[] encoded = {6};
+		int first = 1;
+		System.out.println(Arrays.toString(dxa.decode(encoded, first)));
 	}
 
 }
