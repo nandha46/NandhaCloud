73854c3c9e101032f7411b7792c32d02c3450d3c
Nandha <nandha.kumar790@gmail.com>
2021-02-23 09:59:10 +0530

    added logger to Measure performance

--- Nandha/src/time/MeasurePerformance.java
+++ Nandha/src/time/MeasurePerformance.java
@@ -2,21 +2,29 @@ package time;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.logging.Level;
+
+import util.Loggr;
 /**
  * 
  * @author Nandhakumar Subramanian
  *
  */
 public class MeasurePerformance {
+	private static final String CLASS_NAME = MeasurePerformance.class.getName();
 	// TODO make this class universal & slice into any file to measure performance
 	// TODO Logger into all java codes
 	private void someMethod() {
+		final String methodName = "someMethod";
 		// Some random sleeps and waits..
+		Loggr.logMessage(Loggr.METHOD_ENTRY,Level.INFO, CLASS_NAME, methodName, null);
 		System.out.println("sleeping...");
 		try {
 			Thread.sleep(500l);
 			Thread.sleep(154l);
+			Loggr.logMessage(Loggr.METHOD_EXIT,Level.INFO, CLASS_NAME, methodName, null);
 		} catch (InterruptedException e) {
+			Loggr.logMessage("error",Level.INFO, CLASS_NAME, methodName, null, e);
 			e.printStackTrace();
 		}
 	}
