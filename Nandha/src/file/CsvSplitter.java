7bf3a19868067a825d746bd5fa47afb8a03681e7
Nandha <nandha.kumar790@gmail.com>
2021-02-28 13:29:51 +0530

    improved CsvSplitter java

--- Nandha/src/file/CsvSplitter.java
+++ Nandha/src/file/CsvSplitter.java
@@ -1,20 +1,26 @@
 package file;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+
+import util.Loggr;
 /**
  * @author Nandhakumar Subramanian
  *
  */
 public class CsvSplitter {
-	public static void main(String[] args) throws IOException,FileNotFoundException {
-		File file =new File("/home/local/ZOHOCORP/nandha-con426/Documents/Investing URL/urlsAll.html");
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-		String line; int i=0; int j=0;
+		private static final String CLASS_NAME = CsvSplitter.class.getName();
+		
+	public void split(String fname, BufferedReader reader) {
+		final String methodName = "split";
+		Loggr.logMessage(Loggr.METHOD_ENTRY, Level.SEVERE, CLASS_NAME, methodName, null);
+		String line; 
+		int i=0, j=0;
+		try {
 		PrintWriter writer = new PrintWriter("/home/local/ZOHOCORP/nandha-con426/Documents/Investing URL/urls/page_"+j+".html");
 		while((line =reader.readLine())!= null) {
 			i++; 
@@ -25,6 +31,21 @@ public class CsvSplitter {
 				writer = new PrintWriter("/home/local/ZOHOCORP/nandha-con426/Documents/Investing URL/urls/page_"+j+".html");
 			}
 		}
-		writer.close();	reader.close();
+		writer.close();	
+		reader.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			Loggr.logMessage(Loggr.ERROR, Level.SEVERE, CLASS_NAME, methodName, null, e);
+		}
+		Loggr.logMessage(Loggr.METHOD_EXIT, Level.SEVERE, CLASS_NAME, methodName, null);
+	}
+	
+	public static void main(String[] args) throws IOException,FileNotFoundException {
+		//File file =new File("/home/local/ZOHOCORP/nandha-con426/Documents/Investing URL/urlsAll.html");
+		String fname = "/home/local/ZOHOCORP/nandha-con426/Documents/Investing URL/urlsAll.html";
+		BufferedReader reader = new BufferedReader(new FileReader(fname));
+		CsvSplitter splitter = new CsvSplitter();
+		splitter.split(fname, reader);
 	}
 }
